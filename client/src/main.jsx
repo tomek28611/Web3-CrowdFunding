614b784a78aeda26d2b2177a36d776b4d6e4fad4
Tomek28611 <tomek286@yahoo.com>
2022-12-12 11:43:14 +0100

    client done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import App from './app';
+
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App />
-        </Router>
-
-    </ThirdwebProvider>
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
 )
